7588f7b9cd644d5b05a3e65e112fe4dbaabba921
Ivan <ivo.miyashiro1@gmail.com>
2021-08-10 11:13:25 -0300

    update redirect router

--- src/routes/01-AppRouter.js
+++ src/routes/01-AppRouter.js
@@ -64,7 +64,7 @@ export const AppRouter = () => {
                         component={ NotFoundScreen }
                     />
                     <Redirect
-                        to="/404"
+                        to="/"
                     />
                     
                 </Switch>
